cbffd423d086c8b76b8941f7fac55614414b66a2
hyojin <jihojhi@gmail.com>
2022-05-20 18:21:17 +0900

    RecyclerView Step2

--- app/src/main/java/com/example/imbworks/ui/gallery/GalleryFragment.kt
+++ app/src/main/java/com/example/imbworks/ui/gallery/GalleryFragment.kt
@@ -6,6 +6,8 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.imbworks.adapter.ItemAdapter
+import com.example.imbworks.data.Datasource
 import com.example.imbworks.databinding.FragmentGalleryBinding
 
 class GalleryFragment : Fragment() {
@@ -31,6 +33,16 @@ class GalleryFragment : Fragment() {
 //        galleryViewModel.text.observe(viewLifecycleOwner) {
 //            textView.text = it
 //        }
+
+        // Initialize data.
+        val myDataset = Datasource().loadAffirmations()
+
+        val recyclerView = binding.recyclerView
+        recyclerView.adapter = ItemAdapter(this, myDataset)
+
+        // Use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true)
 
         return root
     }
