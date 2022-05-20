5d04aa2eb093d9f947c16f130710c22b4383b253
hyojin <jihojhi@gmail.com>
2022-05-20 18:10:00 +0900

    RecyclerView Step2

--- app/src/main/java/com/example/imbworks/MainActivity.kt
+++ app/src/main/java/com/example/imbworks/MainActivity.kt
@@ -2,19 +2,16 @@ package com.example.imbworks
 
 import android.os.Bundle
 import android.view.Menu
-import com.google.android.material.snackbar.Snackbar
-import com.google.android.material.navigation.NavigationView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.findNavController
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
-import androidx.drawerlayout.widget.DrawerLayout
-import androidx.appcompat.app.AppCompatActivity
-import androidx.recyclerview.widget.RecyclerView
-import com.example.imbworks.adapter.ItemAdapter
-import com.example.imbworks.data.Datasource
 import com.example.imbworks.databinding.ActivityMainBinding
+import com.google.android.material.navigation.NavigationView
+import com.google.android.material.snackbar.Snackbar
 
 class MainActivity : AppCompatActivity() {
 
